c120bb8c0e80b5592ef10106f33eff8378cfe6ab
Nemanja <nemanjakostovski@gmail.com>
2023-08-26 01:53:48 +0200

    updates to responsivenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,21 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { getTrendingMovies } from '@/lib/fetch';
+import { getTrendingMovies, getTrendingMoviesWeek } from '@/lib/fetch';
 import { useEffect, useState } from 'react';
 import { Carousel } from '@material-tailwind/react';
 
 export default function Home() {
   const [trendingMovies, setTrendingMovies] = useState([]);
+  const [trendingMoviesWeek, setTrendingMoviesWeek] = useState([]);
 
   useEffect(() => {
     async function fetchTrendingMovies() {
       const data = await getTrendingMovies();
+      const weeklyData = await getTrendingMoviesWeek();
+
       setTrendingMovies(data.results);
+      setTrendingMoviesWeek(weeklyData.results);
     }
 
     fetchTrendingMovies();
@@ -25,20 +29,33 @@ export default function Home() {
       <div className='absolute top-0 z-10 w-full sm:w-auto h-60 sm:h-screen pr-10 mb-60'>
         <Carousel
           transition={{ duration: 1 }}
-          className='flex overflow-hidden w-screen sm:w-auto relative'
+          className='flex overflow-hidden w-screen sm:w-auto relative h-screen'
         >
-          {trendingMovies.map(
-            (movie: { backdrop_path: string; title: string; id: string }) => (
+          {trendingMoviesWeek.map(
+            (movie: {
+              backdrop_path: string;
+              poster_path: string;
+              title: string;
+              id: string;
+            }) => (
               <div className='relative' key={movie.id}>
                 <Image
                   src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
                   alt={movie.title}
                   width={1640}
                   height={923}
-                  className='sm:w-auto sm:h-auto w-full pl-4 sm:pl-0 pr-4 sm:pr-0'
+                  className='hidden sm:block sm:w-screen sm:h-screen w-full pl-4 sm:pl-0 pr-4 sm:pr-0'
                   priority
                 />
-                <h3 className='title absolute left-4 bottom-40 mix-blend-exclusion text-white text-xl sm:text-2xl lg:text-4xl decoration-wavy font-bold p-2'>
+                <Image
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  className='sm:hidden w-screen h-screen'
+                  width={384}
+                  height={576}
+                  alt={movie.title}
+                  key={movie.id}
+                />
+                <h3 className='hidden title sm:inline absolute left-4 bottom-40 mix-blend-normal text-white text-xl sm:text-2xl lg:text-4xl decoration-wavy font-bold p-2'>
                   {movie.title}
                 </h3>
               </div>
@@ -47,7 +64,10 @@ export default function Home() {
         </Carousel>
       </div>
       <div className='pt-96 mt-96'>
-        <h2 className='pl-4 mt-32 mb-2 text-xl'>Trending Movies today</h2>
+        <div className='hidden 2xl:block 2xl:h-96'></div>
+        <h2 className='pl-4 mt-32 mb-2 text-xl 2xl:mt-96'>
+          Trending Movies today
+        </h2>
         <div className='flex flex-row overflow-x-scroll'>
           {trendingMovies.map(
             (movie: { poster_path: string; title: string; id: string }) => {
--- src/lib/fetch.tsx
+++ src/lib/fetch.tsx
@@ -22,4 +22,25 @@ async function getTrendingMovies() {
   return data;
 }
 
-export { getTrendingMovies };
+async function getTrendingMoviesWeek() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_ACCESS_TOKEN}`,
+    },
+  };
+
+  const response = await fetch(
+    `${URL}${VERSION}/trending/movie/week?language=en-US`,
+    options
+  );
+  if (response.status !== 200) {
+    throw new Error(`Fetch failed ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+export { getTrendingMovies, getTrendingMoviesWeek };
